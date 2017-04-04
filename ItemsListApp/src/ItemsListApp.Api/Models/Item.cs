dc9aaf558f1b65f6b837f380dd5edf20dc0a145e
Tobias Kamenicky <TobiasK@kentico.com>
2017-04-04 13:46:38 +0200

    KA-121 Rename tests and implement item comparer

--- ItemsListApp/src/ItemsListApp.Api/Models/Item.cs
+++ ItemsListApp/src/ItemsListApp.Api/Models/Item.cs
@@ -7,22 +7,9 @@ namespace ItemsListApp.Api.Models
         public Guid Id { get; set; }
         public string Text { get; set; }
 
-        protected bool Equals(Item other)
+        public override string ToString()
         {
-            return Id.Equals(other.Id);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((Item) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return Id.GetHashCode();
+            return $"{nameof(Id)}: {Id}, {nameof(Text)}: {Text}";
         }
     }
 }
--- ItemsListApp/test/ItemsListApp.Api.UnitTests/Tests/Controllers/ItemsControllerUnitTests.cs
+++ ItemsListApp/test/ItemsListApp.Api.UnitTests/Tests/Controllers/ItemsControllerUnitTests.cs
@@ -7,10 +7,11 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using ItemsListApp.Api.Controllers;
 using ItemsListApp.Api.Models;
+using ItemsListApp.Api.UnitTests.Helpers;
 using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
 
-namespace ItemsListApp.Api.UnitTests
+namespace ItemsListApp.Api.UnitTests.Tests.Controllers
 {
     [TestFixture]
     public class ItemsControllerUnitTests
@@ -28,28 +29,27 @@ namespace ItemsListApp.Api.UnitTests
         }
 
         [Test]
-        public async Task TestGet()
+        public async Task Get_IdOfItem_ReturnsItemWithThisId()
         {
             var expected = new Item
             {
-                Id = Guid.NewGuid(),
+                Id = new Guid("6341EB90-93E6-49AA-BBEC-2A69C3C8DB8D"),
                 Text = "Text of required item",
             };
 
             var action = await _controller.GetAsync(expected.Id);
             var response = await action.ExecuteAsync(CancellationToken.None);
-
             Item actual;
             response.TryGetContentValue(out actual);
 
             Assert.That(response.IsSuccessStatusCode);
-            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actual, Is.EqualTo(expected).UsingItemComparer());
         }
 
         [Test]
-        public async Task TestGetAll()
+        public async Task Get_NoParameters_ReturnsAllItems()
         {
-            var expected = new []
+            var expected = new List<Item>
             {
                 new Item { Id = new Guid("A3672C82-AF6C-44AD-836E-D1C26A0A6359"), Text = "Dummy text 1" },
                 new Item { Id = new Guid("F5CFB0AF-EB26-478B-AF41-7DA314458706"), Text = "Dummy text 2" },
@@ -58,16 +58,15 @@ namespace ItemsListApp.Api.UnitTests
 
             var action = await _controller.GetAsync();
             var response = await action.ExecuteAsync(CancellationToken.None);
-
             IEnumerable<Item> actual;
             response.TryGetContentValue(out actual);
 
             Assert.That(response.IsSuccessStatusCode);
-            Assert.That(actual, Is.EquivalentTo(expected));
+            Assert.That(actual, Is.EqualTo(expected).AsCollection.UsingItemComparer());
         }
 
         [Test]
-        public async Task TestPost()
+        public async Task Post_Text_ReturnsCreatedItem()
         {
             var requestUri = "http://localhost:55036/api/v1/items/";
 
@@ -80,37 +79,35 @@ namespace ItemsListApp.Api.UnitTests
 
             var action = await _controller.PostAsync(expected.Text);
             var response = await action.ExecuteAsync(CancellationToken.None);
-
             Item actual;
             response.TryGetContentValue(out actual);
 
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
-            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actual, Is.EqualTo(expected).UsingItemComparer());
             Assert.That(response.Headers.Location.ToString(), Does.EndWith(expected.Id.ToString()));
 
         }
 
         [Test]
-        public async Task TestPut()
+        public async Task Put_ItemWithId_ReturnsSameItem()
         {
             var expected = new Item
             {
-                Id = Guid.NewGuid(),
+                Id = new Guid("999EA6F0-4139-4D54-B4DD-4976A35D1DFA"),
                 Text = "Text of required item",
             };
 
             var action = await _controller.PutAsync(expected);
             var response = await action.ExecuteAsync(CancellationToken.None);
-
             Item actual;
             response.TryGetContentValue(out actual);
 
             Assert.That(response.IsSuccessStatusCode);
-            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actual, Is.EqualTo(expected).UsingItemComparer());
         }
 
         [Test]
-        public async Task TestDelete()
+        public async Task Delete_IdOfExistingItem_ReturnsNoContentStatusCode()
         {
             var id = Guid.NewGuid();
 
