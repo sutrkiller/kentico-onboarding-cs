17ace359b167f40d923a474c78a56562bc53364f
Tobias Kamenicky <TobiasK@kentico.com>
2017-04-05 14:04:16 +0200

    KA-122 Add location helper and test it

---
+++ ItemsListApp/test/ItemsListApp.Api.UnitTests/Tests/Helpers/ItemLocationHelperUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+using System.Web.Http.Routing;
+using ItemsListApp.Api.Helpers;
+using ItemsListApp.Contracts.Api;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ItemsListApp.Api.UnitTests.Tests.Helpers
+{
+    [TestFixture]
+    public class ItemLocationHelperUnitTests
+    {
+        private IItemLocationHelper _itemLocationHelper;
+        private HttpRequestMessage _httpRequestMessage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _httpRequestMessage = Substitute.For<HttpRequestMessage>();
+            
+            _itemLocationHelper = new ItemLocationHelper(_httpRequestMessage);       
+        }
+
+
+        [Test]
+        public void CreateLocation_Guid_StringEndingWithId()
+        {
+            var guid = new Guid("C648EE26-71CA-4968-9540-AD3470DA0422");
+            ConfigureRequestMessage(guid);
+
+            var location = _itemLocationHelper.CreateLocation(guid);
+
+            Assert.That(location, Does.EndWith(guid.ToString()));
+        }
+
+        private void ConfigureRequestMessage(Guid id)
+        {
+            var configuration = new HttpConfiguration();
+            var route = configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/v1/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            var routeData = new HttpRouteData(route,
+                new HttpRouteValueDictionary
+                {
+                    { "id", id },
+                    { "controller", "Items" }
+                }
+            );
+
+            _httpRequestMessage.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, configuration);
+            _httpRequestMessage.Properties.Add(HttpPropertyKeys.HttpRouteDataKey, routeData);
+        }
+
+
+    }
+}
