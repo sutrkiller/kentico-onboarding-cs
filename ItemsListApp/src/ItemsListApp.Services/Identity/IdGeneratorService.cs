87432e8b35df7e1b09341d7200416892c7731003
Tobias Kamenicky <TobiasK@kentico.com>
2017-04-05 16:00:32 +0200

    KA-122 Add IdGeneratorService and tests

---
+++ ItemsListApp/src/ItemsListApp.Services/Identity/IdGeneratorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ItemsListApp.Services.Identity
+{
+    internal class IdGeneratorService
+    {
+        public Task<Guid> GenerateIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ItemsListApp/test/ItemsListApp.Services.UnitTests/Tests/Identity/IdGeneratorServiceUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using ItemsListApp.Services.Identity;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace ItemsListApp.Services.UnitTests.Tests.Identity
+{
+    [TestFixture()]
+    public class IdGeneratorServiceUnitTests
+    {
+        private IdGeneratorService _idGenerator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _idGenerator = new IdGeneratorService();
+        }
+
+        [Test]
+        public async Task GenerateIdAsync_NotReturnEmptyGuid()
+        {
+            var newId = await _idGenerator.GenerateIdAsync();
+
+            Assert.That(newId, Is.Not.EqualTo(Guid.Empty));
+        }
+
+        [Test]
+        public async Task GenerateIdAsync_TwoConsequentCalls_ReturnUniqueIds()
+        {
+            var newId = await _idGenerator.GenerateIdAsync();
+            var newId2 = await _idGenerator.GenerateIdAsync();
+
+            Assert.That(newId, Is.Not.EqualTo(newId2));
+        }
+    }
+}
