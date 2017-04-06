e7d1820df01dc25e18d4cc4643d6571604bda124
Tobias Kamenicky <TobiasK@kentico.com>
2017-04-06 10:05:41 +0200

    KA-123 Add ConnectionOption to inject connectionString to repo

--- ItemsListApp/src/ItemsListApp.Api/App_Start/DependenciesConfig.cs
+++ ItemsListApp/src/ItemsListApp.Api/App_Start/DependenciesConfig.cs
@@ -1,6 +1,8 @@
-﻿using System.Web.Http;
+﻿using System.Configuration;
+using System.Web.Http;
 using ItemsListApp.Api.DependencyInjection;
 using ItemsListApp.Contracts.DependecnyInjection;
+using ItemsListApp.Contracts.Repository;
 using Microsoft.Practices.Unity;
 
 namespace ItemsListApp.Api
@@ -14,6 +16,13 @@ namespace ItemsListApp.Api
             Register<Repository.DependencyRegister>(container);
             Register<DependencyRegister>(container);
             Register<Services.DependencyRegister>(container);
+
+            container.RegisterInstance(typeof(ConnectionOptions),
+                new ConnectionOptions
+                {
+                    ConnectionString = ConfigurationManager.ConnectionStrings["MongoDbConnection"]?.ConnectionString,
+                    DatabaseName = "items_list_app_db",
+                });
         
             httpConfig.DependencyResolver = new UnityResolver(container);
         }
